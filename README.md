f38c23fa8ebd703cc233f131af553e1835655eb5
Cxevo <ajpnfnwfe@gmail.com>
2022-08-20 14:46:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+README
+im lasy af so replace line 23 players with the names of people in your lobby
---
+++ gui.py
@@ -0,0 +1,38 @@
+import random
+import PySimpleGUI as sg
+
+sg.theme('DarkAmber')
+
+layout = [
+    [sg.Text('Window normal', size=(30, 1), key='Status')],
+    [sg.Text("Press start too randomly select a killer for your lobby")],
+    [sg.Image('Kicon.png')], 
+    [sg.Button('Start',)],
+    [sg.Text('Output', key='Output')],
+]
+
+# create the window
+Window = sg.Window("Dead By daylight kyf rng", layout, resizable=True, finalize=True,)
+Window.bind('<Configure>', "Configure")
+status = Window['Status']
+
+# create an event loop
+while True:
+    event, values = Window.read()
+    if event == 'Start':
+        players = ['Brandon', 'Issac', 'Trenton', 'Steven']
+
+        random_killer = random.choice(players)
+
+        Window['Output'].update(random_killer)
+
+    # events for resize and closing the window
+    if event == sg.WINDOW_CLOSED:
+        break
+    elif event == 'Configure':
+        if Window.TKroot.state() == 'zoomed':
+            status.update(value='Window zoomed and maximized !')
+        else:
+            status.update(value='Window normal')
+
+Window.close()
